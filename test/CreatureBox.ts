b96b4e77caac593e390a6b66a4a4111e242c765c
Alan Chen <alan@maicoin.com>
2021-12-06 16:53:16 +0800

    update

--- test/CreatureBox.ts
+++ test/CreatureBox.ts
@@ -9,19 +9,18 @@ describe("CreatureBox", function () {
   });
 
   it("should do something right", async function () {
-    const { CreatureBox: creatureBoxAddress } = await getNamedAccounts();
+    // const { CreatureBox: creatureBoxAddress } = await getNamedAccounts();
 
-    console.log("Use CreatureBox at:", creatureBoxAddress);
+    // console.log("Use CreatureBox at:", creatureBoxAddress);
+
   
-    // const [address] = process.argv.slice(1);
-  
-    const creatureBox = await ethers.getContractAt("CreatureBox", creatureBoxAddress);
+    // const creatureBox = await ethers.getContractAt("CreatureBox", creatureBoxAddress);
     // const { hash: txHash } = await creatureBox.mint('0x496Bc91e961Ce9eA239994C483FEE30184dB874A');
     // Do something with the accounts
-    // const CreatureBox = await ethers.getContractFactory("CreatureBox");
-    // const creatureBox = await CreatureBox.deploy(500);
+    const CreatureBox = await ethers.getContractFactory("CreatureBox");
+    const creatureBox = await CreatureBox.deploy(500);
 
-    // await creatureBox.deployed();
+    await creatureBox.deployed();
 
     for (let i = 0; i < 200; i++) {
       const tokenId = await creatureBox.computeIndex();
